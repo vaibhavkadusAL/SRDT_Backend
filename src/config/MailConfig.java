3240de616f3d3ea20b0bcfef5623dfade13da62b
Abhishek Mahajan <abhishekmahajan@alphaseam.com>
2025-06-21 16:18:52 +0530

    config

--- src/config/MailConfig.java
+++ src/config/MailConfig.java
@@ -1,25 +1,32 @@
-package com.srdt.contact.config;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 import java.util.Properties;
 
 @Configuration
 public class MailConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
-        sender.setHost("mail.alphaseam.com");
-        sender.setPort(587);
-        sender.setUsername("info.sairuraldevelopmenttrust@gmail.com");
-        sender.setPassword("vyrjuzftuavkshyv");
+        sender.setHost("smtp.gmail.com"); 
+        sender.setPort(587);              
+        sender.setUsername(env.getProperty("spring.mail.username"));
+        sender.setPassword(env.getProperty("spring.mail.password"));
 
         Properties props = sender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "false");
 
         return sender;
     }
